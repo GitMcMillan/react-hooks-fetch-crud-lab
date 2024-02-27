60e18c586b5172d223a61912c4344c757c504331
alexandermcmillan <mcmillancontact@protonmail.com>
2024-02-27 15:20:15 -0800

    completed lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,33 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, setQuestions }) {
+
+
+  function handleCorrectAnswerChange(newIndex) {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ correctIndex: parseInt(newIndex) }),
+    })
+      .then(() => {
+        setQuestions((prevQuestions) => prevQuestions.map((q) => q.id === question.id ? { ...q, correctIndex: parseInt(newIndex) } : q))
+      })
+  }
+
+  function onDeleteClick() {
+    // console.log("DELETING:", question.id)
+
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "DELETE"
+    })
+      .then((r) => r.json())
+      .then(() => {
+        setQuestions((prevQuestions) => prevQuestions.filter((q) => q.id !== question.id))
+      })
+
+
+  }
+
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -15,9 +42,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleCorrectAnswerChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={onDeleteClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,16 @@
 import React from "react";
+import QuestionItem from './QuestionItem'
 
-function QuestionList() {
+function QuestionList({ questions, setQuestions }) {
+
+  const mappedQuestions = questions.map((question) => {
+    return <QuestionItem question={question} key={question.id} setQuestions={setQuestions} />
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{mappedQuestions}</ul>
     </section>
   );
 }
